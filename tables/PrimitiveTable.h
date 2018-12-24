0bc5fd3af73fa098ecd1130939558b7233bbcfbf
EB <eladbitton1@gmail.com>
2018-12-24 16:15:27 +0200

    Lock guard now used

--- tables/PrimitiveTable.h
+++ tables/PrimitiveTable.h
@@ -37,18 +37,16 @@ public:
 
 template<class T>
 void PrimitiveTable<T>::add(const char *key, T value) {
-    lock.lock();
+    lock_guard<mutex> l(lock);
     table[key] = value;
     changed.push_back(key);
-    lock.unlock();
 }
 
 template<class T>
 void PrimitiveTable<T>::add(string key, T value) {
-    lock.lock();
+    lock_guard<mutex> l(lock);
     table[key] = value;
     changed.push_back(key);
-    lock.unlock();
 }
 
 template<class T>
@@ -56,36 +54,32 @@ void PrimitiveTable<T>::update(string key, T v) {
     if (!exists(key)) {
         throw invalid_argument(format("Unable to update missing key %s", key));
     } else {
-        lock.lock();
+        lock_guard<mutex> l(lock);
         table[key] = v;
         changed.push_back(key);
-        lock.unlock();
     }
 }
 
 template<class T>
 vector<string> PrimitiveTable<T>::getChangedKeys() {
-    lock.lock();
+    lock_guard<mutex> l(lock);
     vector<string> changedCopy = changed;
     changed = vector<string>();
-    lock.unlock();
     return changedCopy;
 }
 
 template<class T>
 bool PrimitiveTable<T>::areThereAnyChanges() {
-    lock.lock();
+    lock_guard<mutex> l(lock);
     bool b = !changed.empty();
-    lock.unlock();
     return b;
 }
 
 template<class T>
 T PrimitiveTable<T>::get(string &key) {
     if (exists(key)) {
-        lock.lock();
+        lock_guard<mutex> l(lock);
         T t = table.at(key);
-        lock.unlock();
         return t;
     } else {
         throw invalid_argument("Key is not present in table");
@@ -95,9 +89,8 @@ T PrimitiveTable<T>::get(string &key) {
 
 template<class T>
 bool PrimitiveTable<T>::exists(string &key) {
-    lock.lock();
+    lock_guard<mutex> l(lock);
     bool b = (table.find(key) != table.end());
-    lock.unlock();
     return b;
 }
 
